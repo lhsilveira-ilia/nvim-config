32cc14c5166df949947452094eccc303e69285ce
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-04-13 13:06:49 -0300

    fix neotree floating window open

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,2 +1,23 @@
-vim.keymap.set("n", "<leader>fe", "<Cmd>:Neotree position=float reveal_file=%:p<CR>",
-    { desc = "Open NeoTree file explorer" })
+local neotree = require("neo-tree")
+
+neotree.setup {
+    window = {
+        position = "float",
+
+    },
+}
+
+local function open_explorer()
+    local reveal_file = vim.fn.expand('%:p')
+    if reveal_file == nil then
+        reveal_file = vim.fn.getcwd()
+    end
+
+    require("neo-tree.command").execute({
+        action = "focus",
+        reveal_file = reveal_file
+    })
+end
+
+-- vim.keymap.set("n", "<leader>fe", "<Cmd>:Neotree position=float<CR>", { desc = "Open NeoTree file explorer" })
+vim.keymap.set("n", "<leader>fe", open_explorer, { desc = "Open NeoTree file explorer" })
