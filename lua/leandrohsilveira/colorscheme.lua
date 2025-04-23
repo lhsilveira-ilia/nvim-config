b1ef9963995b6824e0993acf42c5dbd2ebed1ef7
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-04-23 11:13:31 -0300

    add kanagawa colorscheme

--- lua/leandrohsilveira/colorscheme.lua
+++ lua/leandrohsilveira/colorscheme.lua
@@ -1,6 +1,7 @@
 -- vim.cmd("colorscheme tokyonight-night")
 -- vim.cmd("colorscheme omni")
-vim.cmd("colorscheme cyberdream")
+-- vim.cmd("colorscheme cyberdream")
+vim.cmd("colorscheme kanagawa")
 
 -- vim.g.neon_style = "dark"
 -- vim.cmd("colorscheme neon")
