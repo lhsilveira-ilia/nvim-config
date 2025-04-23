534672ba4ce4f9fed48cf5e086c33efa15947c26
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-04-23 10:35:05 -0300

    add lazy vim plugins ui remap

--- lua/leandrohsilveira/remap.lua
+++ lua/leandrohsilveira/remap.lua
@@ -1,16 +1,16 @@
 vim.g.mapleader = " "
 
-local set = vim.keymap.set;
+local set = vim.keymap.set
 
-set('v', '<leader>y', '"+y', { desc = "Yank to clipboard" })
-set('n', '<leader>y', '"+y', { desc = "Yank to clipboard" })
-set('n', '<leader>Y', '"+yg_', { desc = "Yank remaining to end of line to clipboard" })
-set('n', '<leader>yy', '"+yy', { desc = "Yank the entire line to clipboard" })
+set("v", "<leader>y", '"+y', { desc = "Yank to clipboard" })
+set("n", "<leader>y", '"+y', { desc = "Yank to clipboard" })
+set("n", "<leader>Y", '"+yg_', { desc = "Yank remaining to end of line to clipboard" })
+set("n", "<leader>yy", '"+yy', { desc = "Yank the entire line to clipboard" })
 
-set('n', '<leader>w', ':w<CR>', { desc = "Write buffer (Save file)" })
-set('n', '<C-s>', ':w<CR>', { desc = "Write buffer (Save file)" })
-set('n', '<C-S-S>', ':waCR>', { desc = "Write all buffers (Save all files)" })
-set('n', '<leader>W', ':wbd<CR>', { desc = "Write buffer and close it" })
+set("n", "<leader>w", ":w<CR>", { desc = "Write buffer (Save file)" })
+set("n", "<C-s>", ":w<CR>", { desc = "Write buffer (Save file)" })
+set("n", "<C-S-S>", ":waCR>", { desc = "Write all buffers (Save all files)" })
+set("n", "<leader>W", ":wbd<CR>", { desc = "Write buffer and close it" })
 set("n", "<leader>ex", vim.cmd.Ex, { desc = "Open NeoVim builtin file explorer" })
 
 -- keymap set in bufdelete plugin
@@ -31,12 +31,14 @@ set("n", "Q", "<nop>", { desc = "DISABLED: Close NeoVim" })
 
 set("i", "<C-c>", "<Esc>")
 
-set("n", '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-set("n", ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-set("n", 'ld', vim.diagnostic.open_float, { desc = 'Show [L]SP [D]iagnostic messages in a popup' })
-set("n", 'lq', vim.diagnostic.setloclist, { desc = 'Open [L]SP diagnostic [Q]uickfix list' })
+set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+set("n", "ld", vim.diagnostic.open_float, { desc = "Show [L]SP [D]iagnostic messages in a popup" })
+set("n", "lq", vim.diagnostic.setloclist, { desc = "Open [L]SP diagnostic [Q]uickfix list" })
 
 set("n", "<C-k>", "<cmd>cnext<CR>zz")
 set("n", "<C-j>", "<cmd>cprev<CR>zz")
 set("n", "<leader>k", "<cmd>lnext<CR>zz")
 set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+set("n", "<leader>pl", vim.cmd.Lazy, { desc = "Open LazyVim plugins UI" })
