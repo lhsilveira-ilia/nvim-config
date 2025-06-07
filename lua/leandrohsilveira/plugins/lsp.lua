8b4a213fca0d290026894f503b882b2ae9d9a6d6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-06-07 12:19:00 -0300

    add typescriptreact and templ emmet capabilites

--- lua/leandrohsilveira/plugins/lsp.lua
+++ lua/leandrohsilveira/plugins/lsp.lua
@@ -83,6 +83,12 @@ return {
         }
       }
 
+      lspconfig.emmet_ls.setup {
+        -- on_attach = on_attach,
+        capabilities = lspconfig_defaults.capabilities,
+        filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "templ" },
+      }
+
       -- This is where you enable features that only work
       -- if there is a language server active in the file
       vim.api.nvim_create_autocmd("LspAttach", {
